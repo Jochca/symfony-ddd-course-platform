a905da1aece73e78ab6789939789b8257149b4b1
Paweł Chmura <pawel.chmura04@gmail.com>
2025-09-15 20:56:34 +0200

    refactor(Courses): add getters to Course entity

--- src/Courses/Domain/Entity/Course.php
+++ src/Courses/Domain/Entity/Course.php
@@ -15,4 +15,39 @@ final class Course
         private float $price,
     ) {
     }
+
+    public function getId(): CourseId
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    public function changeTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function changeDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function changePrice(float $price): void
+    {
+        $this->price = $price;
+    }
 }
