673d5c223912877a5bffbe8fb9702ec013df1ca3
Paweł Chmura <pawel.chmura04@gmail.com>
2025-09-15 20:02:33 +0200

    chore(ProjectSetup): initialize Symfony project with PHPUnit configuration

---
+++ tests/Courses/Application/SmokeTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Courses\Application;
+
+use PHPUnit\Framework\TestCase;
+
+final class SmokeTest extends TestCase
+{
+    public function testItWorks(): void
+    {
+        $this->assertTrue(true);
+    }
+}
